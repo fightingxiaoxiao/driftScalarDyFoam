e4c0393ff14b3e893c963efc5b4484c663c36b34
Xiaoxiao <smilemax@foxmail.com>
2021-11-15 16:27:21 +0800

    Revert "回退至同时更新p, U, T"
    This reverts commit 21d21aa4c15818dbcff0a75ba733d5bc0ea4b4fe.

--- driftScalarDyFoam.C
+++ driftScalarDyFoam.C
@@ -124,25 +124,37 @@ int main(int argc, char *argv[])
             laminarTransport.correct();
             turbulence->correct();
 
-            // 标量输运
-            // scalar transport
-            solverPerformance Tres;
-            while (simple.correctNonOrthogonal())
+            runTime.printExecutionTime(Info);   // 打印子循环的运行时
+        }
+        runTime.endSubCycle(); //结束子循环
+
+        /*
+        // 检查是否达到收敛
+            if (Tres.initialResidual() < residualT && nStage > 0)
             {
-                #include "TEqn.H"
+                Info << "Subcycle converged." << endl;
+                break; // 跳出子循环
             }
-            
-            // 由于扩散需要获取湍流粘度nut,故检查湍流模型
-            if (!turbulence)
-            {
-                FatalErrorInFunction
-                << "Unable to find turbulence model in the "
-                << "database" << exit(FatalError);
-                return 1;
-            }
+        */
+
+        // 由于扩散需要获取湍流粘度nut,故检查湍流模型
+        if (!turbulence)
+        {
+            FatalErrorInFunction
+            << "Unable to find turbulence model in the "
+            << "database" << exit(FatalError);
+            return 1;
+        }
+
+        // 标量输运
+        // scalar transport
+        solverPerformance Tres;
+        label TCycle = nSubCycles;
+        while (TCycle)
+        {
+            #include "TEqn.H"
 
             volSymmTensorField Reff = turbulence->devReff();
-
             // 修正质量交换率M
             vector zNormal = vector(0.,0.,-1.);
             for (label patchi : snowPatchList)
@@ -152,57 +164,47 @@ int main(int argc, char *argv[])
                 const vectorField &Sfp = mesh.Sf().boundaryField()[patchi];         // 面积矢量
                 const scalarField &magSfp = mesh.magSf().boundaryField()[patchi];   // 面积矢量模长
                 const scalarField &Tp = T.boundaryField()[patchi];                  // 边界处的雪漂浓度
-
-                const symmTensorField& Reffp = Reff.boundaryField()[patchi];        
+                const symmTensorField& Reffp = Reff.boundaryField()[patchi];
 
                 vectorField& ssp = wallShearStress.boundaryFieldRef()[patchi];
+                ssp = (-Sfp/magSfp) & Reffp;    // 剪切应力
 
-                ssp = (-Sfp/magSfp) & Reffp;    // 剪切应力 
-
-                
                 scalarField UShear = sqrt(mag(ssp)/rhoAir);         // 剪切风速模量
-
-                scalarField& Mp = M.boundaryFieldRef()[patchi];
+                scalarField &Mp = M.boundaryFieldRef()[patchi];
+                scalarField &deltaHp = deltaH.boundaryFieldRef()[patchi];
 
                 // 更新质量交换率（侵蚀/沉积）
                 // update mass exchange rate on snow surface (erosion & deposition)
-                Info << "Update mass exchange rate." << endl; 
+                Info << "Update mass exchange rate." << endl;
                 forAll(Mp, i)
                 {
                     const scalar zArea = Sfp[i] & zNormal;
                     if (UShear[i] > Uthreshold) // 侵蚀
                     {
-                        //Mp[i] = -ca * rhoSnow * UShear[i] * (1.-sqr(Uthreshold)/sqr(UShear[i])) * zArea;
+                        //Mp[i] = -ca * rhoSnow * UShear[i] * (1.-sqr(Uthreshold)/sqr(UShear[i])) *zArea;
                         Mp[i] = -ca * rhoSnow * (sqr(UShear[i]) - sqr(Uthreshold)) * zArea;
                     }
                     else // 沉积
                     {
-                        Mp[i] = Tp[i] * mag(wf.value()) * (1 - sqr(UShear[i])/sqr(Uthreshold))* zArea;
+                        Mp[i] = Tp[i] * mag(wf.value()) * (1 - sqr(UShear[i])/sqr(Uthreshold)) * zArea;
                     }
-
                     //tmpDeltaH[i] = Mp[i] / rhoSnow / zArea * runTime.deltaTValue();
+                    deltaHp[i] = Mp[i] / rhoSnow / zArea;
                 }
             }
-
-            runTime.printExecutionTime(Info);   // 打印子循环的运行时
-
-            // 检查是否达到收敛
-            if (Tres.initialResidual() < residualT && nStage > 0)
-            {   
-                Info << "Subcycle converged." << endl;
-                break; // 跳出子循环
-            }
+            // 计算根据M计算deltaH
+            TCycle--;
         }
-        runTime.endSubCycle(); //结束子循环
 
-        // 计算根据M计算deltaH
-        vector zNormal = vector(0.,0.,-1.);
+
+        /*
+
         for (label patchi : snowPatchList)
         {
-            scalarField &deltaHp = deltaH.boundaryFieldRef()[patchi];
+
 
             const scalarField &Mp = M.boundaryField()[patchi];
-            const vectorField &Sfp = mesh.Sf().boundaryField()[patchi]; 
+            const vectorField &Sfp = mesh.Sf().boundaryField()[patchi];
 
             forAll(Mp, i)
             {
@@ -210,13 +212,13 @@ int main(int argc, char *argv[])
                 deltaHp[i] = Mp[i] / rhoSnow / zArea;
             }
         }
-        
+        */
         runTime.write();
 
         ++nStage;
     }
 
-    
+
 
     return 0;
 }
